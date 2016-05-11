643495f7033d862eace35e7bcc9ebce80fb2919d
Administrator <Administrator@A211-31.pslib.cz>
2016-05-11 12:39:09 +0200

    První pokus

---
+++ src/Program.py
@@ -0,0 +1,10 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+import time
+
+for i in range(0,10):
+    print "Cyklus:",i
+    time.sleep(0.1)
+    
+    
